eeac49d87b24af2fc942fc8c6b36d44620ede0da
Rafal Malinowski <cyrax1321@gmail.com>
2023-05-27 23:54:37 +0200

    make separate function that calculates and writes root to reduce code repeatinion

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,24 @@
 import { indexInput, inputRootCanvas, outputRootCanvas, radicantInput } from "./selectors.js";
 import { drawRoot, resizeRootCanvasAndRadicantInput } from "./rootCanvas.js";
 import { calculateRoot } from "./calculator.js";
-import { getIndexAndRadicant, writeRootOutput } from "./helperFunctions";
+import { getIndexAndRadicant, writeRootOutput } from "./helperFunctions.js";
 drawRoot(inputRootCanvas);
 drawRoot(outputRootCanvas);
 radicantInput.addEventListener("keydown", e => {
     setTimeout(() => {
         resizeRootCanvasAndRadicantInput(radicantInput, inputRootCanvas);
-        const { index, radicant } = getIndexAndRadicant();
-        if (index && radicant) {
-            const root = calculateRoot(index, radicant);
-            writeRootOutput(root);
-        }
+        calculateAndWriteRoot();
     }, 0);
 });
 indexInput.addEventListener("keydown", e => {
     setTimeout(() => {
-        const { index, radicant } = getIndexAndRadicant();
-        if (index && radicant) {
-            const root = calculateRoot(index, radicant);
-            writeRootOutput(root);
-        }
+        calculateAndWriteRoot();
     }, 0);
 });
+function calculateAndWriteRoot() {
+    const { index, radicant } = getIndexAndRadicant();
+    if (index && radicant) {
+        const root = calculateRoot(index, radicant);
+        writeRootOutput(root);
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,11 @@ import {
      indexInput, 
      inputRootCanvas, 
      outputRootCanvas, 
-     radicantInput,
-     multiplierOutput,
-     indexOutput,
-     radicantOutput 
+     radicantInput
     } from "./selectors.js";
 import {drawRoot, resizeRootCanvasAndRadicantInput} from "./rootCanvas.js";
 import { calculateRoot } from "./calculator.js";
-import { getIndexAndRadicant, writeRootOutput } from "./helperFunctions";
+import { getIndexAndRadicant, writeRootOutput } from "./helperFunctions.js";
 
 drawRoot(inputRootCanvas);
 drawRoot(outputRootCanvas);
@@ -17,22 +14,21 @@ drawRoot(outputRootCanvas);
 radicantInput.addEventListener("keydown", e=>{
     setTimeout(()=>{
         resizeRootCanvasAndRadicantInput(radicantInput, inputRootCanvas);
-        const {index, radicant}=getIndexAndRadicant();
-        if(index&&radicant){
-            const root=calculateRoot(index, radicant);
-            writeRootOutput(root);
-        } 
+        calculateAndWriteRoot();
     }, 0);
 });
 
 indexInput.addEventListener("keydown", e=>{
     setTimeout(()=>{
-        const {index, radicant}=getIndexAndRadicant();
-        if(index&&radicant){
-            const root=calculateRoot(index, radicant);
-            writeRootOutput(root);
-        } 
+        calculateAndWriteRoot();
     }, 0);
 });
 
+function calculateAndWriteRoot(){
+    const {index, radicant}=getIndexAndRadicant();
+    if(index&&radicant){
+        const root=calculateRoot(index, radicant);
+        writeRootOutput(root);
+    } 
+}
 
