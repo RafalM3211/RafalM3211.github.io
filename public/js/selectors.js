a24f210fa2652ca56ae5e94810eb15f849cd236b
Rafal Malinowski <cyrax1321@gmail.com>
2023-05-29 21:30:04 +0200

    remove old js files in favor of one bundle js file

--- public/js/selectors.js
+++
@@ -1,8 +0,0 @@
-export const inputRootCanvas = document.querySelector("#input-root-canvas");
-export const outputRootCanvas = document.querySelector("#output-root-canvas");
-export const indexInput = document.querySelector(".index");
-export const radicantInput = document.querySelector(".radicant");
-export const multiplierOutput = document.querySelector(".multiplier-output");
-export const errorOutput = document.querySelector(".error-output");
-export const indexOutput = document.querySelector(".index.root-output");
-export const radicantOutput = document.querySelector(".radicant.root-output");
